79bf2cb38ccecaf285b450e5148419fed616348f
jeremyben-meir <jsb459@cornell.edu>
2022-08-22 21:20:30 -0700

    recency update

--- src/assets/populate.js
+++ src/assets/populate.js
@@ -1,9 +1,6 @@
 import Pdf from '../../src/assets/Ben_Meir_Resume.pdf';
 
-export var bio = `Hi, I am Jeremy Ben-Meir, a fourth-year at Cornell University, majoring in Computer Science, 
-and minoring in Real Estate and Data Science. I am interested in machine learning, data science,
-real estate investing, and blockchain applications.
-`
+export var bio = `My name is Jeremy Ben-Meir, and I recently graduated from a Cornell University, where I earned a B.S. degree in Computer Science (with minors in Real Estate and Data Science). I am interested in machine learning, data science, real estate investing, and blockchain applications.`
 
 export var sectioning = {
     // "Incoming Experience": [
@@ -13,7 +10,7 @@ export var sectioning = {
       {
         "title": "Google",
         "link": null,
-        "pos": "Incoming Software Engineer"
+        "pos": "Software Engineer"
       }
     ],
     "Previous Experiences": [
@@ -67,17 +64,27 @@ export var sectioning = {
             "link": "https://jeremyben-meir.github.io/word-puzzle",
             "pos": null
         },
+        {
+          "title": "Artist Portfolio Website [Not Mobile Optimized]",
+          "link": "https://samben-meir.com/",
+          "pos": null
+        },
+        {
+          "title": "Home Portfolio Website [Incomplete]",
+          "link": "https://www.stevenroseproperties.com/",
+          "pos": null
+        },
     ],
     "Contact": [
       {
         "title": "Email",
         "link": null,
-        "pos": "jsb459 \"at\" cornell \"dot\" edu"
+        "pos": "jsb459@cornell.edu"
       },
       {
         "title": "Phone",
         "link": null,
-        "pos": "917 697 3521"
+        "pos": "+1 (917) 697-3521"
       }
     ],
     
