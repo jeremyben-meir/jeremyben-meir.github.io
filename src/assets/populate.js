0dd5d4e047e647c43b5f25fd20928eacd5f549c6
jeremyben-meir <jsb459@cornell.edu>
2023-04-05 00:13:12 -0700

    updated resume

--- src/assets/populate.js
+++ src/assets/populate.js
@@ -1,92 +1,94 @@
-import Pdf from '../../src/assets/Ben_Meir_Resume.pdf';
+import Pdf from "../../src/assets/Ben_Meir_Resume.pdf";
 
-export var bio = `My name is Jeremy Ben-Meir, and I recently graduated from a Cornell University, where I earned a B.S. degree in Computer Science (with minors in Real Estate and Data Science). I am interested in machine learning, data science, real estate investing, and blockchain applications.`
+export var bio = `My name is Jeremy Ben-Meir, and I recently graduated from a Cornell University, where I earned a B.S. degree in Computer Science (with minors in Real Estate and Data Science). I am interested in machine learning, data science, real estate investing, and blockchain applications.`;
 
 export var sectioning = {
-    // "Incoming Experience": [
+  // "Incoming Experience": [
 
-    // ],
-    "Current Experience": [
-      {
-        "title": "Google",
-        "link": null,
-        "pos": "Software Engineer"
-      }
-    ],
-    "Previous Experiences": [
-      {
-        "title": "Open Storefront Directory",
-        "link": "https://jeremyben-meir.github.io/open-storefront-directory/",
-        "pos": "Lead Data Scientist"
-      },
-      {
-        "title": "Cornell Blockchain",
-        "link": "https://jeremyben-meir.github.io/metaland-frontend/",
-        "pos": "Research and Development Team Member"
-      },
-      {
-        "title": "Amazon",
-        "link": null,
-        "pos": "Software Development Engineer Intern"
-      },{
-        "title": "Survision",
-        "link": null,
-        "pos": "Application Developer"
-      },{
-        "title": "Cornell Robotic Personal Assistants Laboratory",
-        "link": null,
-        "pos": "Research Assistant"
-      }
-    ],
-    "Relevant Links": [
-      {
-        "title": "LinkedIn",
-        "link": "https://www.linkedin.com/in/jeremy-ben-meir/",
-        "pos": null
-      },{
-        "title": "GitHub",
-        "link": "https://github.com/jeremyben-meir/",
-        "pos": null
-      },{
-        "title": "Resume",
-        "link": Pdf,
-        "pos": null
-      }
-    ],
-    "Hobby Projects": [
-        {
-            "title": "Magazine Design",
-            "link": "https://issuu.com/jeremy.ben-meir",
-            "pos": null
-        },
-        {
-            "title": "Word Puzzle",
-            "link": "https://jeremyben-meir.github.io/word-puzzle",
-            "pos": null
-        },
-        {
-          "title": "Artist Portfolio Website [Not Mobile Optimized]",
-          "link": "https://samben-meir.com/",
-          "pos": null
-        },
-        {
-          "title": "Home Portfolio Website [Incomplete]",
-          "link": "https://www.stevenroseproperties.com/",
-          "pos": null
-        },
-    ],
-    "Contact": [
-      {
-        "title": "Email",
-        "link": null,
-        "pos": "jsb459@cornell.edu"
-      },
-      {
-        "title": "Phone",
-        "link": null,
-        "pos": "+1 (917) 697-3521"
-      }
-    ],
-    
-
-  }
+  // ],
+  "Current Experience": [
+    {
+      title: "Google",
+      link: null,
+      pos: "Software Engineer",
+    },
+  ],
+  "Previous Experiences": [
+    {
+      title: "Amazon",
+      link: null,
+      pos: "Software Development Engineer Intern",
+    },
+    {
+      title: "Cornell Blockchain",
+      link: "https://jeremyben-meir.github.io/metaland-frontend/",
+      pos: "Research and Development Team Member",
+    },
+    {
+      title: "Open Storefront Directory",
+      link: "https://jeremyben-meir.github.io/open-storefront-directory/",
+      pos: "Lead Data Scientist",
+    },
+    {
+      title: "Survision",
+      link: null,
+      pos: "Application Developer",
+    },
+    {
+      title: "Cornell Robotic Personal Assistants Laboratory",
+      link: null,
+      pos: "Research Assistant",
+    },
+  ],
+  "Relevant Links": [
+    {
+      title: "LinkedIn",
+      link: "https://www.linkedin.com/in/jeremy-ben-meir/",
+      pos: null,
+    },
+    {
+      title: "GitHub",
+      link: "https://github.com/jeremyben-meir/",
+      pos: null,
+    },
+    {
+      title: "Resume",
+      link: Pdf,
+      pos: null,
+    },
+  ],
+  "Hobby Projects": [
+    {
+      title: "Magazine Design",
+      link: "https://issuu.com/jeremy.ben-meir",
+      pos: null,
+    },
+    {
+      title: "Word Puzzle",
+      link: "https://jeremyben-meir.github.io/word-puzzle",
+      pos: null,
+    },
+    {
+      title: "Artist Portfolio Website [Not Mobile Optimized]",
+      link: "https://samben-meir.com/",
+      pos: null,
+    },
+    {
+      title: "Home Portfolio Website [Incomplete]",
+      link: "https://www.stevenroseproperties.com/",
+      pos: null,
+    },
+  ],
+  Contact: [
+    {
+      title: "Email",
+      link: null,
+      pos: "jsb459@cornell.edu",
+    },
+    {
+      title: "Phone",
+      link: null,
+      pos: "+1 (917) 697-3521",
+    },
+  ],
+};
